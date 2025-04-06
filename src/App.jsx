2f906f9555dc5501591f301e9ff3606b6101e2ca
Abdulbasit <abdulbasitkayode@gmail.com>
2025-04-06 21:02:53 +0100

    feat: implement routing and add main page components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import Services from "./Pages/Services/Services";
+import Blog from "./Pages/Blog/Blog";
+import Contact from "./Pages/Contact/Contact";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/contct" element={<Contact />} />
+        </Routes>
+      </Router>
+    </>
+  );
+};
 
-export default App
+export default App;
